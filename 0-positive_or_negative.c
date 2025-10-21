fd3d66d52d85eb91ba6b995d69e79d4361778d26
rhilesfekkai <ramirhiles@gmail.com>
2025-10-21 14:48:43 +0000

    e

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Fonction principale
+ *
+ * Description : Affiche si le nombre stocké dans n est positif, nul ou négatif
+ *
+ * Return: 0 (succès)
+ */
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+
+    return (0);
+}
+
