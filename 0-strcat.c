df5caf9adb7585b3067d38f01e45dfbae49e5fa1
rhilesfekkai <ramirhiles@gmail.com>
2025-11-01 23:07:26 +0000

    Add task 0: implement _strcat function

---
+++ 0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    int i = 0;
+    int j = 0;
+
+    /* find the end of dest */
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+
+    /* copy src to the end of dest */
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return (dest);
+}
+
