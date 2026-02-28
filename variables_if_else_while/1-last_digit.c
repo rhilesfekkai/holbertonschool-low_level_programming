b239353a8a1f3268a3c6fa5f5b2307c13d851b12
rhilesfekkai <ramirhiles@gmail.com>
2026-02-28 13:47:10 +0000

    Task 1: C program that prints the last digit of a number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,20 +9,20 @@
  */
 int main(void)
 {
-    int n, last;
+	int n, last;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    last = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
-    printf("Last digit of %d is %d ", n, last);
+	printf("Last digit of %d is %d ", n, last);
 
-    if (last > 5)
-        printf("and is greater than 5\n");
-    else if (last == 0)
-        printf("and is 0\n");
-    else
-        printf("and is less than 6 and not 0\n");
+	if (last > 5)
+		printf("and is greater than 5\n");
+	else if (last == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 
-    return (0);
+	return (0);
 }
