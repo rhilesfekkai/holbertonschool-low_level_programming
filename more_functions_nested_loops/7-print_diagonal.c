295d1abcda78723f893f7ba694711033a090a97a
rhilesfekkai <ramirhiles@gmail.com>
2026-03-02 20:55:01 +0000

    Fix 7-print_diagonal.c: pass Betty style

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,19 +8,19 @@
  */
 void print_diagonal(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < i; j++)
-            _putchar(' ');
-        _putchar('\\');
-        _putchar('\n');
-    }
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
