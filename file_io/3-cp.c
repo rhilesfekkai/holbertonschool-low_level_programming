b798ac2ba953281da3b2a84241e21d922581b6dd
rhilesfekkai <ramirhiles@gmail.com>
2026-08-25 10:43:01 +0000

    Fix Betty style for file copy

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,6 +4,37 @@
 #include <stdio.h>
 
 /**
+ * copy_file - copies the content of one file to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @name: destination file name
+ * Return: 0 on success, 98 or 99 on failure
+ */
+int copy_file(int fd_from, int fd_to, char *name)
+{
+	char buffer[1024];
+	int r, w;
+
+	while ((r = read(fd_from, buffer, 1024)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+			return (99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		return (98);
+	}
+
+	return (0);
+}
+
+/**
  * main - copies the content of a file to another file
  * @ac: argument count
  * @av: argument vector
@@ -11,8 +42,7 @@
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, r, w, close_from, close_to;
-	char buffer[1024];
+	int fd_from, fd_to, status;
 
 	if (ac != 3)
 	{
@@ -35,35 +65,21 @@ int main(int ac, char **av)
 		return (99);
 	}
 
-	while ((r = read(fd_from, buffer, 1024)) > 0)
+	status = copy_file(fd_from, fd_to, av[2]);
+	if (status != 0)
 	{
-		w = write(fd_to, buffer, r);
-		if (w != r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			close(fd_from);
-			close(fd_to);
-			return (99);
-		}
-	}
-
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		close(fd_from);
 		close(fd_to);
-		return (98);
+		return (status);
 	}
 
-	close_from = close(fd_from);
-	if (close_from == -1)
+	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		return (100);
 	}
 
-	close_to = close(fd_to);
-	if (close_to == -1)
+	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		return (100);
